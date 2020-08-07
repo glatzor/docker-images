f247c1bd131b4bd7f7df55a0c243ebfa07578c92
Sebastian Heinlein <devel@glatzor.de>
2020-08-07 23:05:38 +0200

    php: make xdebug work on macos

---
+++ php/files/docker-entrypoint-xebug-on-macos.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Native networking is not availabe using MacOS. So we have to adapt 
+# the xdebug configuration
+
+INI=<<EOF
+[xdebug]
+xdebug.remote_connect_back=off
+xdebug.remote_host=host.docker.internal
+EOF
+
+# If the host system uses a linuxkit kernel we are likely running on docker tools
+if [[ $(uname --kernel-release) =~ ^[0-9]+\.[0-9]+\.[0-9]+-linuxkit$ ]]; then
+	for PHP_ETC_BASE in /etc/php/*; do
+		echo ${INI} > ${PHP_ETC_BASE}/cli/conf.d/99-docker.ini
+		echo ${INI} > ${PHP_ETC_BASE}/fpm/conf.d/99-docker.ini
+	done
+fi

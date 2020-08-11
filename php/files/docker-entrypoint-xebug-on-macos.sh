5b05425666e2357e81a68e66c3ec79b72702f85f
Sebastian Heinlein <devel@glatzor.de>
2020-08-11 20:48:36 +0200

    php: respect newlines in ini file var

--- php/files/docker-entrypoint-xebug-on-macos.sh
+++ php/files/docker-entrypoint-xebug-on-macos.sh
@@ -9,7 +9,7 @@ xdebug.remote_host=host.docker.internal"
 # If the host system uses a linuxkit kernel we are likely running on docker tools
 if [[ $(uname --kernel-release) =~ ^[0-9]+\.[0-9]+\.[0-9]+-linuxkit$ ]]; then
 	for PHP_ETC_BASE in /etc/php/*; do
-		echo ${INI} > ${PHP_ETC_BASE}/cli/conf.d/99-docker.ini
-		echo ${INI} > ${PHP_ETC_BASE}/fpm/conf.d/99-docker.ini
+		echo "${INI}" > ${PHP_ETC_BASE}/cli/conf.d/99-docker.ini
+		echo "${INI}" > ${PHP_ETC_BASE}/fpm/conf.d/99-docker.ini
 	done
 fi
